52b54c908fc6ad010b8ec4aa1e754d2a3724768f
chenkai-wds <122259337+chenkai-wds@users.noreply.github.com>
2023-04-18 11:53:53 +0800

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+import com.wds.UI.GameJFrame;
+import com.wds.UI.LoginJFrame;
+import com.wds.UI.RegisterJFrame;
+
+public class App {
+    public static void main(String[] args)
+    {
+        //new LoginJFrame();
+        //new RegisterJFrame();
+        new GameJFrame();
+    }
+
+}
